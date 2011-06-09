16a04140b85fae5fd61bb3efda0880bd2559cf38
Paul Merrill <napalminc@gmail.com>
2011-06-09 02:08:47 -0700

    TileTypes load after first row

--- src/area.cpp
+++ src/area.cpp
@@ -191,8 +191,8 @@ bool Area::processTileset(xmlNode* node)
 Area::TileType Area::defaultTileType(const Gosu::Bitmap source, coord_t tiledim,
                                int id)
 {
-	int x = tiledim.x * (id % source.width());
-	int y = tiledim.y * (id / source.height());
+	int x = (tiledim.x * id) % source.width();
+	int y = (tiledim.y * id) / source.height() * tiledim.y; // ???
 	
 	TileType tt;
 	Gosu::Image* img = rc->bitmapSection(source, x, y,
