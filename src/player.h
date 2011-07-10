c1a317656facb36226d2069977b3cbc6c268371c
Paul Merrill <napalminc@gmail.com>
2011-07-10 16:25:11 -0700

    add player.h

---
+++ src/player.h
@@ -0,0 +1,25 @@
+/******************************
+** Tsunagari Tile Engine     **
+** player.h                  **
+** Copyright 2011 OmegaSDG   **
+******************************/
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "entity.h"
+
+class Player : public Entity
+{
+public:
+	Player(Resourcer* rc, Area* area, const std::string& descriptor);
+
+	//! Move the entity by dx, dy.
+	void moveByTile(coord_t delta);
+
+private:
+	void postMove();
+};
+
+#endif
+
