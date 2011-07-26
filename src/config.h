dd23cd7f39d38fde40bb444a9aaf0f30374e871a
Michael D. Reiley <mreiley@omegasdg.com>
2011-07-25 17:11:24 -0700

    Fix M_PI declaration on VisualC++

--- src/config.h
+++ src/config.h
@@ -49,6 +49,12 @@
 	#ifdef _MSC_VER
 		#define snprintf _snprintf
 	#endif
+
+	/* Fix PI declaration. */
+	#ifndef M_PI
+		#define M_PI 3.14159265358979323846
+		#define M_PI_2 1.57079632679489661923
+	#endif
 // ===
 
 #endif
