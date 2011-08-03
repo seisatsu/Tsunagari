548c8d7f7b715b8824bd00853acf04263435b560
Paul Merrill <napalminc@gmail.com>
2011-08-03 00:42:54 -0700

    comment script

--- src/postMove.lua
+++ src/postMove.lua
@@ -1,17 +1,21 @@
 #!/usr/bin/lua
-print("Lua says: I'm at x " .. x .. ", y " .. y)
+--
+-- This script is executed every time an entity finishes walking onto a tile.
+--
+-- We are called from within Entity::postMove() in entity.cpp. There, we are
+-- passed some game data. We pass three variables to this script from the Entity
+-- object: x, y, and entity, which represents the Entity itself.
+--
+-- We also have a C function we can call, gotoUpperLeft, which takes an Entity
+-- object as a parameter.
+--
 
+print("Lua says: Entity is at " .. x .. ", " .. y)
+
+-- The red portal teleports us! *gasp* Excitement!
 if x == 11 and y == 4 then
 	gotoUpperLeft(entity)
+	-- The x,y variables in this script aren't updated yet, but they will
+	-- be next time we're called.
 end
 
--- script.lua
--- Receives a table, returns the sum of its components.
---io.write("The table the script received has:\n");
---x = 0
---for i = 1, #foo do
---  print(i, foo[i])
---  x = x + foo[i]
---end
---io.write("Returning data back to C\n");
---return x
