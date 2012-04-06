8a9674bd340701fe4504a4aafc3e2b42d574f57a
Michael D. Reiley <mreiley@omegasdg.com>
2012-04-05 23:31:00 -0700

    Cool sounds in testing.world and player animation speed change.

--- src/testing/areas/house_armor.py
+++ src/testing/areas/house_armor.py
@@ -1,1 +1,5 @@
-Sound.play("sounds/metal_clank.oga") # armor sound
+import random
+
+snd = Sound.play("sounds/metal_clank.oga") # armor sound
+snd.speed = 1.0 + (float(random.randint(-3, 3)) / 10)
+
