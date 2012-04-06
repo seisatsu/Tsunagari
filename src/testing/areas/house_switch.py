fe30c24fdc996b1f268ccb411e2b8a76f9acd51a
Paul Merrill <napalminc@gmail.com>
2012-04-05 21:58:06 -0700

    factor out TileBase

--- src/testing/areas/house_switch.py
+++ src/testing/areas/house_switch.py
@@ -4,7 +4,7 @@ if unlocked_the_door == False:
 
     tile = area.tiles(4, 0, 0.0) # closed exit on north wall, property layer
     tile.exit = newExit("areas/secret_room.tmx", 4, 5, 0.0)
-    tile.walkable = True
+    tile.flags.nowalk = False
 
     tile = area.tiles(4, 0, -0.2) # closed exit on north wall, graphics layer
     tile.type = area.get_tile_type(66) # change to open exit
