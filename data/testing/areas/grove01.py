066dc1d09f67f5eeacf63be84338493f5b2ce6dc
Paul Merrill <napalminc@gmail.com>
2012-06-08 03:50:44 -0700

    bug fix: grove01 well had broken variables

--- data/testing/areas/grove01.py
+++ data/testing/areas/grove01.py
@@ -3,13 +3,13 @@ def grove01_load():
 	grove01_ai_wizard = stdlib_ai_wander("entities/wizard/wizard.xml", "down", 6, 3, 0.0, 1.0, 4)
 
 def grove01_update():
-	global grove01_drinking
+	global grove01_drinking, grove01_duration, grove01_max_alpha
 	if grove01_drinking == True:
-		progress = grove01_well_timer.count / duration
+		progress = grove01_well_timer.count / grove01_duration
 		if progress < 0.5:
-			overlay_alpha = int(2 * max_alpha * progress)
+			overlay_alpha = int(2 * grove01_max_alpha * progress)
 		elif progress < 1.0:
-			overlay_alpha = int(2 * max_alpha * (1 - progress))
+			overlay_alpha = int(2 * grove01_max_alpha * (1 - progress))
 		else:
 			overlay_alpha = 0
 			grove01_drinking = False
