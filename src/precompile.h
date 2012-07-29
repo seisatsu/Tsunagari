2e7f8b6713107bdfa8b7ac409eb54fb68529fd4b
Paul Merrill <napalminc@gmail.com>
2012-07-28 18:52:25 -0700

    add precompile.h

---
+++ src/precompile.h
@@ -0,0 +1,39 @@
+#ifdef USE_PRECOMPILED_HEADERS
+
+#include <algorithm>
+#include <boost/algorithm/string.hpp>
+#include <boost/foreach.hpp>
+#include <boost/format.hpp>
+#include <boost/optional.hpp>
+#include <boost/python.hpp>
+#include <boost/scoped_array.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/unordered_map.hpp>
+#include <boost/unordered_set.hpp>
+#include <boost/utility.hpp>
+#include <boost/variant.hpp>
+#include <cstring>
+#include <ctime>
+#include <ctype.h>
+#include <deque>
+#include <errno.h>
+#include <Gosu/Gosu.hpp>
+#include <iostream>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+#include <list>
+#include <map>
+#include <math.h>
+#include <physfs.h>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include <time.h>
+#include <vector>
+
+#endif
+
